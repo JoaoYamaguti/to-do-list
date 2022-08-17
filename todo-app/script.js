d3ef4f065903f419748ea12e46c01b3c31d395d0
JoaoYamaguti <105678854+JoaoYamaguti@users.noreply.github.com>
2022-08-17 00:38:16 -0300

    make delete button

--- todo-app/script.js
+++ todo-app/script.js
@@ -1,26 +1,32 @@
 const btnForm = document.querySelector('.btnform')
 const txtForm = document.querySelector('#todoTxt')
+const listTodo = document.querySelector('.list-todo')
+const tasks = []
 
-function add() {
-    const listTodo = document.querySelector('.list-todo')
-    
+let cont = 0
+
+
+btnForm.addEventListener('click', () => {   
     const isEmpty = txtForm.value === ""
     if (!isEmpty) {
-        listTodo.innerHTML += `
-        <li>
+        tasks.push(`
+        <li id="${cont}">
             <input type="checkbox"  class="checkList">
             <input type="text" name="txtList" class="txtList" value="${txtForm.value}">
-            <button class="btnlist">Delete</button>
+            <button class="btnlist" onclick="btndel(${cont})">Delete</button>
         </li>
-        `        
+        `)       
         txtForm.value = ""
         txtForm.focus()
+        listTodo.innerHTML = ""
+        for (let i = 0; i < tasks.length; i++) {
+            listTodo.innerHTML += tasks[i]   
+        }
     } else {
         alert('text your task!!!')
     }
-    }
-
-btnForm.addEventListener('click', () => {add()})
+    cont++  
+})
 txtForm.addEventListener('keypress', function(event) {
     if (event.key === "Enter") {
         // Cancel the default action, if needed
@@ -30,3 +36,17 @@ txtForm.addEventListener('keypress', function(event) {
     }
     console.log(event)
 })
+
+console.log(btnDelete)
+
+
+function btndel(idnum) {
+    tasks[idnum] = ""
+    listTodo.innerHTML = ""
+    for (let i = 0; i < tasks.length; i++) {
+        listTodo.innerHTML += tasks[i]
+    }
+    console.log(tasks)
+}
+
+
