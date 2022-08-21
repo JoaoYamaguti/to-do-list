d00407da536c7c10bb70e3144bbaed27279f8c18
JoaoYamaguti <105678854+JoaoYamaguti@users.noreply.github.com>
2022-08-20 23:10:23 -0300

    passando mal

--- todo-app/script.js
+++ todo-app/script.js
@@ -1,25 +1,23 @@
-const btnForm = document.querySelector('.btnform')
 const txtForm = document.querySelector('#todoTxt')
-const listTodo = document.querySelector('.list-todo')
+const btnForm = document.querySelector('.btnform')
 const tasks = []
+const listTodo = document.querySelector('.list-todo')
+
+function showList() {
+    listTodo.innerHTML = ""
+    tasks.forEach(element => {
+        listTodo.innerHTML += element.tags
+    })
+}
 
 btnForm.addEventListener('click', () => {   
     const isEmpty = txtForm.value === ""
-    let cont = (tasks.length + 1)
     if (!isEmpty) {
-        tasks.push(`
-        <li>
-            <input type="checkbox"  class="checkList">
-            <span class="txtList">${txtForm.value}</span>
-            <button class="btnlist" onclick="btndel(${cont})">Delete</button>
-        </li>
-        `)       
+        tasks.push({id : tasks.length, tags : `<li><input type="checkbox" class="checkList"><span class="txtList">${txtForm.value}</span><button class="btnlist" onclick="deleteLi(${tasks.length})">Delete</button></li>`})
         txtForm.value = ""
         txtForm.focus()
-        listTodo.innerHTML = ""
-        for (let i = 0; i < tasks.length; i++) {
-            listTodo.innerHTML += tasks[i]   
-        }
+        showList()
+        console.log(tasks)
     } else {
         alert('text your task!!!')
     }  
@@ -31,14 +29,70 @@ txtForm.addEventListener('keypress', function(event) {
         // Trigger the button element with a click
         btnForm.click();
     }
-    console.log(event)
 })
 
-function btndel(idnum) {
-    tasks[idnum - 1] = ""
-    listTodo.innerHTML = ""
-    for (let i = 0; i < tasks.length; i++) {
-        listTodo.innerHTML += tasks[i]
-    }
-    console.log(tasks)
+
+function deleteLi () {
+    console.log()
+
+    const btnList = document.querySelectorAll('.btnList')
+    btnList.forEach(item => {
+        const txtItem = item.parentNode.innerHTML
+        const txthtml = `<li>${txtItem}</li>`
+        console.log(txthtml)
+        const indextasks = tasks.indexOf(txthtml)
+        console.log(indextasks)
+        tasks.splice(indextasks, 1)
+        showList()
+    })
 }
+
+listTodo.addEventListener('change', (event) =>{
+    console.log(event)
+
+    const btnsDelete = document.querySelectorAll('.btnlist')
+    console.log(btnsDelete)
+
+    btnsDelete.forEach(item => {
+        console.log(item)
+        item.addEventListener('click', () => {
+            const txtItem = item.parentNode.innerHTML
+            const txthtml = `<li>${txtItem}</li>`
+            console.log(txthtml)
+            const indextasks = tasks.indexOf(txthtml)
+            console.log(indextasks)
+            tasks.splice(indextasks, 1)
+            showList()
+        })
+    })
+
+    // const checkTask = document.querySelectorAll('.checkList')
+    // checkTask.forEach(item => {
+    //     item.addEventListener('change', () => {
+
+    //         const txtItem = item.parentNode.innerHTML
+    //         const txthtml = `<li>${txtItem}</li>`
+    //         const indextasks = tasks.indexOf(txthtml)
+    
+    //         const itemCheck = item.checked
+    //         if(itemCheck) {
+    //             const strg = txthtml.split('<input type="checkbox" class="checkList">')
+    //             strg.splice(1, 0, '<input type="checkbox" class="checkList" checked>')
+    //             let txtli = ''
+    //             strg.forEach(i =>{
+    //                 txtli += i
+    //             })
+    //             tasks[indextasks] = txtli
+    //         } else {
+    //             const strg = txthtml.split('<input type="checkbox" class="checkList" checked>')
+    //             strg.splice(1, 0, '<input type="checkbox" class="checkList">')
+    //             let txtli = ''
+    //             strg.forEach(i =>{
+    //                 txtli += i
+    //             })
+    //             tasks[indextasks] = txtli
+    //         }
+    //     })
+    // })
+ 
+})
