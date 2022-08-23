6a23ffea0d5aa562ae56c29ba0ad0fa59df90a7a
JoaoYamaguti <105678854+JoaoYamaguti@users.noreply.github.com>
2022-08-22 22:04:55 -0300

    fix js

--- todo-app/script.js
+++ todo-app/script.js
@@ -1,98 +1,40 @@
-const txtForm = document.querySelector('#todoTxt')
-const btnForm = document.querySelector('.btnform')
-const tasks = []
-const listTodo = document.querySelector('.list-todo')
-
-function showList() {
-    listTodo.innerHTML = ""
-    tasks.forEach(element => {
-        listTodo.innerHTML += element.tags
-    })
-}
-
-btnForm.addEventListener('click', () => {   
-    const isEmpty = txtForm.value === ""
-    if (!isEmpty) {
-        tasks.push({id : tasks.length, tags : `<li><input type="checkbox" class="checkList"><span class="txtList">${txtForm.value}</span><button class="btnlist" onclick="deleteLi(${tasks.length})">Delete</button></li>`})
-        txtForm.value = ""
-        txtForm.focus()
-        showList()
-        console.log(tasks)
-    } else {
-        alert('text your task!!!')
-    }  
-})
-txtForm.addEventListener('keypress', function(event) {
-    if (event.key === "Enter") {
-        // Cancel the default action, if needed
-        event.preventDefault();
-        // Trigger the button element with a click
-        btnForm.click();
-    }
-})
-
-
-function deleteLi () {
-    console.log()
-
-    const btnList = document.querySelectorAll('.btnList')
-    btnList.forEach(item => {
-        const txtItem = item.parentNode.innerHTML
-        const txthtml = `<li>${txtItem}</li>`
-        console.log(txthtml)
-        const indextasks = tasks.indexOf(txthtml)
-        console.log(indextasks)
-        tasks.splice(indextasks, 1)
-        showList()
-    })
-}
-
-listTodo.addEventListener('change', (event) =>{
-    console.log(event)
-
-    const btnsDelete = document.querySelectorAll('.btnlist')
-    console.log(btnsDelete)
-
-    btnsDelete.forEach(item => {
-        console.log(item)
-        item.addEventListener('click', () => {
-            const txtItem = item.parentNode.innerHTML
-            const txthtml = `<li>${txtItem}</li>`
-            console.log(txthtml)
-            const indextasks = tasks.indexOf(txthtml)
-            console.log(indextasks)
-            tasks.splice(indextasks, 1)
-            showList()
-        })
-    })
-
-    // const checkTask = document.querySelectorAll('.checkList')
-    // checkTask.forEach(item => {
-    //     item.addEventListener('change', () => {
-
-    //         const txtItem = item.parentNode.innerHTML
-    //         const txthtml = `<li>${txtItem}</li>`
-    //         const indextasks = tasks.indexOf(txthtml)
+window.addEventListener('load', () =>{
+    const form = document.querySelector('#formAdd')
+    const txtForm = document.querySelector('#txtForm')
+    const listTodo = document.querySelector('.list-todo')
     
-    //         const itemCheck = item.checked
-    //         if(itemCheck) {
-    //             const strg = txthtml.split('<input type="checkbox" class="checkList">')
-    //             strg.splice(1, 0, '<input type="checkbox" class="checkList" checked>')
-    //             let txtli = ''
-    //             strg.forEach(i =>{
-    //                 txtli += i
-    //             })
-    //             tasks[indextasks] = txtli
-    //         } else {
-    //             const strg = txthtml.split('<input type="checkbox" class="checkList" checked>')
-    //             strg.splice(1, 0, '<input type="checkbox" class="checkList">')
-    //             let txtli = ''
-    //             strg.forEach(i =>{
-    //                 txtli += i
-    //             })
-    //             tasks[indextasks] = txtli
-    //         }
-    //     })
-    // })
- 
+    form.addEventListener('submit', (e) => {   
+        e.preventDefault()
+        const isEmpty = txtForm.value == ""
+
+        if (!isEmpty) {
+            const li = document.createElement('li')
+            
+            const inputcheck = document.createElement('input')
+            inputcheck.type ='checkbox'
+            inputcheck.classList.add('checkList')
+            li.appendChild(inputcheck)
+
+            const inputtxt = document.createElement('input')
+            inputtxt.type = 'text'
+            inputtxt.classList.add('txtList')
+            inputtxt.setAttribute('readonly', 'readonly')
+            inputtxt.value = txtForm.value
+            li.appendChild(inputtxt)
+
+            const btnDel = document.createElement('button')
+            btnDel.type = 'button'
+            btnDel.innerText = 'Delete'
+            btnDel.classList.add('btnlist')
+            li.appendChild(btnDel)
+
+            listTodo.appendChild(li)
+
+            btnDel.addEventListener('click', ()=>{
+                listTodo.removeChild(li)
+            })
+        } else {
+            alert('type your task!!!')
+        }
+    })
 })
